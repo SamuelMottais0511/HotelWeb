5ce62a44e2d1f02916944efcfe775deebcad70cb
SamuelMottais0511 <samuelmottais@MacBook-Pro-de-Samuel.local>
2017-06-06 21:32:48 +0200

    Change

--- server.js
+++ server.js
@@ -8,13 +8,19 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('client'));
 
-app.get('/chat/', function (req, res) {
+app.get('/', function (req, res) {
     var serverPick = "Cool",
         playerPick = req.params.playerChoice;
     // res.send(confrontation(serverPick,playerPick));
     res.render('game',{gameResult:"You are the champion."});
 })
-    
+
+app.get('/samuel', function (req, res) {
+    var serverPick = "Cool",
+        playerPick = req.params.playerChoice;
+    // res.send(confrontation(serverPick,playerPick));
+    res.render('chambres',{gameResult:"You are the champion."});
+})    
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
