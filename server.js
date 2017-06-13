c8c89706f7ca4427b4a1ce10e1360f8c2febb2b4
SamuelMottais0511 <samuelmottais@MacBook-Pro-de-Samuel.local>
2017-06-14 00:21:53 +0200

    Site web quasi fini

--- server.js
+++ server.js
@@ -9,20 +9,26 @@ app.set('view engine', 'handlebars');
 app.use(express.static('client'));
 
 app.get('/chambres', function (req, res) {
-    var serverPick = "Cool",
-        playerPick = req.params.playerChoice;
-    // res.send(confrontation(serverPick,playerPick));
+
     res.render('chambres',{gameResult:"You are the champion."});
 })
 
-app.get('/samuel', function (req, res) {
-    var serverPick = "Cool",
-        playerPick = req.params.playerChoice;
-    // res.send(confrontation(serverPick,playerPick));
-    res.render('chambres',{gameResult:"You are the champion."});
+app.get('/hotelparis', function (req, res) {
+
+    res.render('accueil',{gameResult:"You are the champion."});
 })    
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/visite', function (req, res) {
+
+    res.render('visite',{gameResult:"You are the champion."});
+})   
+
+app.get('/restaurant', function (req, res) {
+    
+    res.render('restaurant',{gameResult:"You are the champion."});
+})  
+
+app.listen(2929, function () {
+  console.log('Example app listening on port 2929!')
 })
 
