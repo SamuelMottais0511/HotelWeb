0daca26c17f970aac6497bdaa60242fae5205f81
Gilles Camille <b00571069@essec.edu>
2017-06-20 17:03:42 +0200

    change Camille

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser');
+
 
 var app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
@@ -13,7 +20,7 @@ app.get('/chambres', function (req, res) {
     res.render('chambres',{gameResult:"You are the champion."});
 })
 
-app.get('/hotelparis', function (req, res) {
+app.get('/accueil', function (req, res) {
 
     res.render('accueil',{gameResult:"You are the champion."});
 })    
@@ -27,6 +34,16 @@ app.get('/restaurant', function (req, res) {
     
     res.render('restaurant',{gameResult:"You are the champion."});
 })  
+
+app.get('/contact', function (req, res) {
+    
+    res.render('contact',{gameResult:"You are the champion."});
+});
+
+app.post('/contact', function (req, res) {
+    console.log(req.body);
+    res.send("Merci");
+});
 
 app.listen(2929, function () {
   console.log('Example app listening on port 2929!')
